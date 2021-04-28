a7bb84c78ed4c482373402ce6e9510078f8e805d
imvickykumar999 <imvickykumar999@gmail.com>
2021-04-28 20:12:24 +0530

    switch crud

--- main.py
+++ main.py
@@ -6,34 +6,80 @@ import json
 firebase_obj = firebase.FirebaseApplication('https://home-automation-336c0-default-rtdb.firebaseio.com/', None)
 
 def pull():
-    result = firebase_obj.get('/esp32/led/', None)
+    result = firebase_obj.get('/esp32/switch/led/', None)
 
     with open("data.json", "w") as outfile:
         json.dump(result, outfile)
 
     print('Value fetched = ', result)
 
-
 def push(data):
-    firebase_obj.put('/esp32','led', data)
+    firebase_obj.put('/esp32/switch/','led', data)
     print('updated')
 
-def insert():
-    result = firebase_obj.post('/esp32/led/', data)
+def insert(data):
+    result = firebase_obj.post('/esp32/switch/', data)
     print(result)
 
 def remove(result):
-    firebase.delete('/esp32/led/', result)
+    firebase_obj.delete('/esp32/switch/', result)
     print('deleted')
 
+# def switch(argument):
+#     switcher = {
+#         1: push(data),
+#         2: pull(),
+#         3: insert(data),
+#         4: remove(result),
+#     }
+#
+#     return switcher.get(argument, "Try Again...")
+
+# output = switch(choice)
 
 if __name__ == "__main__":
 
-    data = { 'Name': 'LED',
-              'Value': 1,
-              'Switch': 'ON',
-              'Cost': 100
-            }
+    try:
+        with open('data.json', 'r') as openfile:
+          data = json.load(openfile)
+    except Exception as e:
+        print('''\n   No JSON file present...
+        Try Calling Pull method.
+        ''')
 
-    push(data)
-    pull() 
+    print('''
+    Menu...
+
+        1: push(data)
+        2: pull()
+        3: insert(data)
+        4: remove(result)
+        0: ...Exit
+    ''')
+
+    choice = True
+    output = None
+
+    while int(choice):
+        choice = input('\n  Enter your Choice : ')
+
+        if choice == '1':
+            output = push(data)
+        elif choice == '2':
+            output = pull()
+        elif choice == '3':
+            output = insert(data)
+        elif choice == '4':
+            result = input('\nEnter child name : ')
+            output = remove(result)
+        else:
+            print('Exit...')
+
+        print(output)
+
+    # push(data)
+    # pull()
+
+    # result = insert(data)
+    # if input("Want to delete ? (1/0) : "):
+    #     remove(result)
